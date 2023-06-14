f2d6a7e81bdc8e41fa64467cc935ddcbd1eae866
Yash Singh <yashsingh0666@gmail.com>
2023-06-14 20:21:26 +0530

    express-basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http  = require('http');
+
+const server = http.createServer((req,res)=>{
+console.log('user hit the server')
+res.end('home page ( here we have used this to show that all the content has been sent and so that the page could be loaded)')
+})
+
+server.listen(5000)
